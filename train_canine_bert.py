19132287d6f98012207ac92775dcf1a195568b52
Cristian Simionescu <cristian@nexusmedia.ro>
2022-11-17 09:05:21 +0200

    Misc

--- train_canine_bert.py
+++ train_canine_bert.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from canine_bert_model import DiacCanineBertTokenClassification
 from utils import PreprocessingUtils
 
-CACHE_PREPROCESSING = False
+CACHE_PREPROCESSING = True
 if __name__ == '__main__':
     wandb.login()
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
 
     dataset = load_dataset("dumitrescustefan/diacritic")
-    dataset["train"] = dataset["train"].select(list(range(3000000)))
+    dataset["train"] = dataset["train"].select(list(range(20000000)))
     dataset["validation"] = dataset["validation"]#.select(list(range(1000)))
     train_ds = dataset
     train_ds = train_ds.rename_column("text", "labels")
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     per_label_weights = [n_samples / (c * n_samples) for c in per_label_counts]
     per_label_weights = [w * 1/max(per_label_weights) for w in per_label_weights]
     print("Label weights", per_label_weights)
+    # per_label_weights = [1, 1, 1, 1]
 
     test_ds = train_ds["validation"]
     train_ds = train_ds["train"]
@@ -47,16 +48,16 @@ if __name__ == '__main__':
 
     TRAIN_BATCH_SIZE = 64
     TEST_BATCH_SIZE = 64
-    LR = 1e-3
+    LR = 1e-5
     EPOCHS = 100
 
     train_dataloader = DataLoader(train_ds, batch_size=TRAIN_BATCH_SIZE, shuffle=True, drop_last=True, num_workers=4, persistent_workers=True)
     test_dataloader = DataLoader(test_ds, batch_size=TEST_BATCH_SIZE, drop_last=False, num_workers=4, persistent_workers=True)
 
     checkpoint_callback = ModelCheckpoint(dirpath="checkpoints_canine_bert", save_top_k=-1, monitor="validation_loss")
-    model = DiacCanineBertTokenClassification(num_labels=len(utils.labels), per_label_weights=per_label_weights, lr=LR)
-    # model = DiacCanineBertTokenClassification.load_from_checkpoint('checkpoints2/epoch=1-step=156250.ckpt', strict=True, num_labels=len(utils.labels), per_label_weights=per_label_weights, lr=LR)
+    # model = DiacCanineBertTokenClassification(num_labels=len(utils.labels), per_label_weights=per_label_weights, lr=LR)
+    model = DiacCanineBertTokenClassification.load_from_checkpoint('checkpoints1/epoch=0-step=23437.ckpt', strict=True, num_labels=len(utils.labels), per_label_weights=per_label_weights, lr=LR)
     wandb_logger = WandbLogger(name='canine-c_bert-base', project='Diacritic')
-    trainer = Trainer(accelerator='gpu',precision='bf16', amp_backend="native",devices=2, logger=wandb_logger, callbacks=[checkpoint_callback], max_epochs=EPOCHS, accumulate_grad_batches=8)
-    trainer.fit(model, train_dataloader, test_dataloader)
-    # trainer.fit(model, train_dataloader, test_dataloader, ckpt_path='checkpoints2/epoch=1-step=156250.ckpt')
+    trainer = Trainer(accelerator='gpu',precision='bf16', amp_backend="native",devices=2, logger=wandb_logger, callbacks=[checkpoint_callback], max_epochs=EPOCHS, accumulate_grad_batches=1)
+    # trainer.fit(model, train_dataloader, test_dataloader)
+    trainer.fit(model, train_dataloader, test_dataloader, ckpt_path='checkpoints1/epoch=0-step=23437.ckpt')
